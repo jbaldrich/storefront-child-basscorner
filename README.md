5fcd8698fc54534f941d140bd750e66ebcb8e23b
Jaco Baldrich <jb@jacobaldrich.com>
2018-10-23 17:36:37 +0200

    starter commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Storefront Child Theme for Tanne Bass Corner
+
+This is a custom Sorefront Child Theme made for Tanne Bass Corner
+
+## Installation
+
+1. Download the child theme from GitHub Repository.
+2. Goto WordPress > Appearance > Themes > Add New.
+2. Click Upload Theme and Choose File, then select the theme's .zip file. Click Install Now.
+3. Click Activate to use your new theme right away.
+
+## Usage
+
+This child theme is designed to be used as a starter theme for the WooCommerce StoreFront theme which you can download for free below.
+
+* [Download WooCommerce StoreFront Theme](https://wordpress.org/themes/storefront/)
+* [StoreFront Documentation](http://docs.woocommerce.com/documentation/themes/storefront/)
+* [StoreFront Child Themes](https://woocommerce.com/product-category/themes/storefront-child-theme-themes/)
+* [StoreFront Extensions](https://woocommerce.com/product-category/storefront-extensions/)
+
+Custom PHP that you write should be added to the child themes functions.php file whilst any custom CSS should be added to the child themes style.css file.
---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Storefront automatically loads the core CSS even if using a child theme as it is more efficient
+ * than @importing it in the child theme style.css file.
+ *
+ * Uncomment the line below if you'd like to disable the Storefront Core CSS.
+ *
+ * If you don't plan to dequeue the Storefront Core CSS you can remove the subsequent line and as well
+ * as the sf_child_theme_dequeue_style() function declaration.
+ */
+//add_action( 'wp_enqueue_scripts', 'sf_child_theme_dequeue_style', 999 );
+
+/**
+ * Dequeue the Storefront Parent theme core CSS
+ */
+function sf_child_theme_dequeue_style() {
+    wp_dequeue_style( 'storefront-style' );
+    wp_dequeue_style( 'storefront-woocommerce-style' );
+}
+
+/**
+ * Note: DO NOT! alter or remove the code above this text and only add your custom PHP functions below this text.
+ */
+
+/**
+ * Place product search bar before secondary navigation
+ */
+function jbr_change_search_secondary_nav_order(){
+	remove_action('storefront_header','storefront_secondary_navigation',30);
+	remove_action('storefront_header','storefront_product_search',40);
+	add_action('storefront_header','storefront_secondary_navigation',40);
+	add_action('storefront_header','storefront_product_search',30);
+}
+add_action('init','jbr_change_search_secondary_nav_order');
+
